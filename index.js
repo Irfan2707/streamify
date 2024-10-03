e8a3273a40186dc10aebc41c97addf6a045382e2
Your Name <irfanhusain2707@gmail.com>
2024-10-04 00:55:39 +0530

    modified the index file

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get("/youtube", (req , res)=>{
     res.send("<h1>This is youtube link </h1>")
 })
 
+app.get("/twitter", (req , res)=>{
+    res.send("<h1>This is twitter link </h1>")
+})
+
 app.listen(process.env.PORT, ()=>{
     console.log('App is listening on port ${port}')
 })
